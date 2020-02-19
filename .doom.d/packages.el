d1927b14afa1e6d8937a2c5edf533538e3535113
Sebastien Hurtel <sebastien@bruno.blade-group.com>
2020-02-19 14:55:55 +0100

    [doom] Add uuid package
    Use uuid package for junos-mode.

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -31,6 +31,8 @@
 (package! junos-mode
   :recipe (:host github :repo "vincentbernat/junos-mode"))
 
+(package! uuid)
+
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
